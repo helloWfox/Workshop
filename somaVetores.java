9e26267fa40537bd05880e1ffe6320c431be6f3a
AMANDA FERNANDA LOPES MARTINS <17amandamartins@gmail.com>
2026-06-03 10:52:38 -0300

    adicionando o arquivo somaVetorores.java

---
+++ somaVetores.java
@@ -0,0 +1,12 @@
+public class somaVetores {
+    public static void main(String[] args) {
+        int[] vetor = {10, 20, 30, 40, 50};
+        int soma = 0;
+        
+        for (int i = 0; i < vetor.length; i++) {
+            soma += vetor[i]; 
+        }
+        
+        System.out.println("A soma dos elementos é: " + soma);
+    }
+}
